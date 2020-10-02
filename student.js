10a085e01aabc218b271e6d9c56a8c2a36645eaa
Srish-tii <srishtilodha2000@gmail.com>
2020-10-02 20:36:11 +0530

    added pages

---
+++ student.js
@@ -0,0 +1,10 @@
+var x=document.getElementById("no-mems");
+var y=document.getElementById("guidance")
+function func1(){
+    x.style.display = "block";
+    y.style.display="none";
+}
+function func2(){
+    x.style.display = "none";
+    y.style.display = "block";
+}
